04ddafb0c4955be56d7faa792360357ec58ec936
Bruce Anderson <anderson.bruce.b@gmail.com>
2017-03-09 17:29:35 -0500

    Move to typescript

---
+++ src/xtal-browser-window.ts
@@ -0,0 +1,86 @@
+declare module Polymer{
+    export abstract class Element{
+        connectedCallback();
+        ready();
+        $;
+        children: any[];
+    }
+}
+/** @polymerElement */
+class XtalBrowserWindow extends Polymer.Element {
+    static get is() { return 'xtal-browser-window'; }
+    static get properties(){
+        return {
+            linkOnly:{
+                type: Boolean
+            },
+            sandBox:{
+                type: String
+            },
+            windowTop:{
+                type: Number
+            },
+            width:{
+                type: String,
+                value: '90%'
+            },
+            collapsedText:{
+                type: String,
+                value: '►'
+            },
+            clicked:{
+                type: Boolean,
+                notify: true,
+                reflectToAttribute: true
+            } 
+        }
+
+    }
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        super.connectedCallback();
+    }
+    ready(){
+        super.ready();
+        var aLink = this.children[0];
+        aLink.target = '_blank';
+        if(this['sandBox']){
+            var iFrame = this.$.iframe;
+            iFrame.setAttribute('sandbox', this['sandBox']);
+        }
+    }
+    handleOnLoad(){
+        this.$.div_iframe.scrollTop = this['windowTop'];
+    }
+    markClicked(){
+        this['clicked'] = true;
+    }
+    toggleIFrame(){
+        this['clicked'] = true;
+        var aLink = this.children[0];
+        if(this['linkOnly']){
+            window.open(aLink.href);
+            return;
+        }
+        var iFrame = this.$.iframe;
+        var toggleButton = this.$.toggle_button;
+        var divIFrame = this.$.div_iframe;
+        var div_noFrame = this.$.div_noFrame;
+        if(divIFrame.style.display ==='none'){
+            divIFrame.style.display = 'block';
+            div_noFrame.style.display = 'none';
+            
+            iFrame.src = aLink.href;
+            toggleButton.innerText = '▼';
+        }else{
+            iFrame.src='about:blank';
+            divIFrame.style.display = 'none';
+            div_noFrame.style.display = 'block';
+            toggleButton.innerText = this['collapsedText'];
+        }
+        
+    }
+}
+customElements.define(XtalBrowserWindow.is, XtalBrowserWindow);
