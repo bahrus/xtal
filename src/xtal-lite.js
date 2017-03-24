b6a70bcc06e0a4b91924a61187b600aee7ceb9cb
Bruce Anderson <anderson.bruce.b@gmail.com>
2017-03-23 21:58:27 -0400

    xtal

--- src/xtal-lite.js
+++ src/xtal-lite.js
@@ -2,11 +2,42 @@ var xtal;
 (function (xtal) {
     xtal.domLite = {};
     class XtalLite extends HTMLElement {
+        constructor() {
+            super(...arguments);
+            this.dashedChildren = {};
+        }
         connectedCallback() {
             const id = this.tagName.toLowerCase();
             const template = xtal.domLite[id];
             var clone = document.importNode(template.content, true);
-            this.appendChild(clone);
+            const nd = this.appendChild(clone);
+            this.getDashedTagNames(this);
+            // customElements.whenDefined('xtal-fetch').then(() =>{
+            //     console.log(customElements.get('xtal-fetch'));
+            // });
+        }
+        getDashedTagNames(nd) {
+            if (!nd.hasChildNodes)
+                return;
+            for (let i = 0, cn = nd.children, ii = cn.length; i < ii; i++) {
+                console.log('i = ' + i);
+                const childNd = cn[i];
+                let tagName = childNd.tagName;
+                const dc = this.dashedChildren;
+                if (tagName.indexOf('-') > 0) {
+                    tagName = tagName.toLowerCase();
+                    if (typeof dc[tagName] === 'undefined') {
+                        dc[tagName] = null;
+                        customElements.whenDefined(tagName).then(() => {
+                            const construct = customElements.get(tagName);
+                            console.log(construct);
+                        });
+                    }
+                }
+                else {
+                    this.getDashedTagNames(childNd);
+                }
+            }
         }
     }
     xtal.XtalLite = XtalLite;
--- src/xtal-lite.ts
+++ src/xtal-lite.ts
@@ -1,11 +1,38 @@
 module xtal{
     export const domLite:{[key: string] : HTMLTemplateElement} = {}
     export class XtalLite extends HTMLElement{
+        dashedChildren: {[key: string] : Function} = {};
         connectedCallback() {
             const id = this.tagName.toLowerCase();
             const template = xtal.domLite[id] as HTMLTemplateElement;
             var clone = document.importNode(template.content, true);
-            this.appendChild(clone);
+            const nd = this.appendChild(clone);
+            this.getDashedTagNames(this);            
+            // customElements.whenDefined('xtal-fetch').then(() =>{
+            //     console.log(customElements.get('xtal-fetch'));
+            // });
+        }
+        getDashedTagNames(nd: HTMLElement){
+            if(!nd.hasChildNodes) return;
+            for(let i = 0, cn = nd.children, ii = cn.length; i < ii; i++){
+                console.log('i = ' + i);
+                const childNd = cn[i] as HTMLElement;
+                let tagName = childNd.tagName;
+                const dc = this.dashedChildren;
+                if(tagName.indexOf('-') > 0){
+                    tagName = tagName.toLowerCase();
+                    if(typeof dc[tagName] === 'undefined'){
+                        dc[tagName] = null;
+                        customElements.whenDefined(tagName).then(() =>{
+                            const construct = customElements.get(tagName);
+                            console.log(construct);
+                        })
+                    }
+                   
+                }else{
+                    this.getDashedTagNames(childNd);
+                }
+            }
         }
     }
     customElements.define('xtal-lite', XtalLite);
