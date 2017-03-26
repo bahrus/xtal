11cf9a62c6e3cefda6ea9a6e42059f8a2e06abed
Bruce Anderson <anderson.bruce.b@gmail.com>
2017-03-26 10:17:17 -0400

    xtal edge

--- src/xtal-fetch.js
+++ src/xtal-fetch.js
@@ -2,62 +2,72 @@ var xtal;
 (function (xtal) {
     var elements;
     (function (elements) {
-        class XtalFetch extends Polymer.Element {
-            constructor() {
-                super(...arguments);
-                this.as = 'text';
-            }
-            static get is() { return 'xtal-fetch'; }
-            static get properties() {
-                return {
-                    /**
-                     * Possible values are 'text' and 'json'
-                     */
-                    as: {
-                        type: String
-                    },
-                    debounceTimeInMs: {
-                        type: Number
-                    },
-                    reqInfo: {
-                        type: Object,
-                    },
-                    reqInit: {
-                        type: Object
-                    },
-                    insertResults: {
-                        type: Boolean
-                    },
-                    href: {
-                        type: String,
-                        observer: 'loadNewUrl'
-                    },
-                    /**
-                     * The expression for where to place the result.
-                     */
-                    result: {
-                        type: Object,
-                        notify: true,
-                        readOnly: true
-                    },
-                };
-            }
-            loadNewUrl() {
-                if (this.href) {
-                    const _this = this;
-                    fetch(this.href).then(resp => {
-                        resp[_this.as]().then(val => {
-                            _this['_setResult'](val);
-                            if (typeof val === 'string' && this.insertResults) {
-                                this.innerHTML = val;
-                            }
-                            //_this.notifyPath('result');
+        function initXtalFetch() {
+            class XtalFetch extends Polymer.Element {
+                constructor() {
+                    super(...arguments);
+                    this.as = 'text';
+                }
+                static get is() { return 'xtal-fetch'; }
+                static get properties() {
+                    return {
+                        /**
+                         * Possible values are 'text' and 'json'
+                         */
+                        as: {
+                            type: String
+                        },
+                        debounceTimeInMs: {
+                            type: Number
+                        },
+                        reqInfo: {
+                            type: Object,
+                        },
+                        reqInit: {
+                            type: Object
+                        },
+                        insertResults: {
+                            type: Boolean
+                        },
+                        href: {
+                            type: String,
+                            observer: 'loadNewUrl'
+                        },
+                        /**
+                         * The expression for where to place the result.
+                         */
+                        result: {
+                            type: Object,
+                            notify: true,
+                            readOnly: true
+                        },
+                    };
+                }
+                loadNewUrl() {
+                    if (this.href) {
+                        const _this = this;
+                        fetch(this.href).then(resp => {
+                            resp[_this.as]().then(val => {
+                                _this['_setResult'](val);
+                                if (typeof val === 'string' && this.insertResults) {
+                                    this.innerHTML = val;
+                                }
+                                //_this.notifyPath('result');
+                            });
                         });
-                    });
+                    }
                 }
             }
+            customElements.define(XtalFetch.is, XtalFetch);
         }
-        customElements.define(XtalFetch.is, XtalFetch);
+        function waitForPolymerElement() {
+            if (typeof Polymer === 'undefined' || Polymer.Element === undefined) {
+                setTimeout(waitForPolymerElement, 50);
+                return;
+            }
+            initXtalFetch();
+        }
+        waitForPolymerElement();
     })(elements = xtal.elements || (xtal.elements = {}));
 })(xtal || (xtal = {}));
 //# sourceMappingURL=xtal-fetch.js.map
--- src/xtal-fetch.ts
+++ src/xtal-fetch.ts
@@ -8,66 +8,73 @@ module xtal.elements{
         result: any | polymer.PropObjectType,
         insertResults: boolean | polymer.PropObjectType,
     }
-    class XtalFetch extends Polymer.Element implements IXtalFetchProperties{
-        reqInfo: RequestInfo;
-        reqInit: RequestInit;
-        href: string;
-        result: object;
-        as = 'text';
-        debounceTimeInMs: number;
-        insertResults: boolean;
-        static get is(){return 'xtal-fetch';}
-        static get properties() : IXtalFetchProperties{
-            return {
-                /**
-                 * Possible values are 'text' and 'json'
-                 */
-                as:{
-                    type: String
-                },
-                debounceTimeInMs:{
-                    type: Number
-                },
-                reqInfo: {
-                    type: Object,
-                },
-                reqInit:{
-                    type: Object
-                },
-                insertResults:{
-                    type: Boolean
-                },
-                href:{
-                    type: String,
-                    observer: 'loadNewUrl'
-                },
-                /**
-                 * The expression for where to place the result.
-                 */
-                 result:{
-                    type: Object,
-                    notify: true,
-                    readOnly: true
-                },
+    function initXtalFetch(){
+        class XtalFetch extends Polymer.Element implements IXtalFetchProperties{
+            reqInfo: RequestInfo;
+            reqInit: RequestInit;
+            href: string;
+            result: object;
+            as = 'text';
+            debounceTimeInMs: number;
+            insertResults: boolean;
+            static get is(){return 'xtal-fetch';}
+            static get properties() : IXtalFetchProperties{
+                return {
+                    /**
+                     * Possible values are 'text' and 'json'
+                     */
+                    as:{
+                        type: String
+                    },
+                    debounceTimeInMs:{
+                        type: Number
+                    },
+                    reqInfo: {
+                        type: Object,
+                    },
+                    reqInit:{
+                        type: Object
+                    },
+                    insertResults:{
+                        type: Boolean
+                    },
+                    href:{
+                        type: String,
+                        observer: 'loadNewUrl'
+                    },
+                    /**
+                     * The expression for where to place the result.
+                     */
+                    result:{
+                        type: Object,
+                        notify: true,
+                        readOnly: true
+                    },
+                }
             }
-        }
-        loadNewUrl(){
-            if(this.href){
-                const _this = this;
-                fetch(this.href).then(resp =>{
-                    resp[_this.as]().then(val =>{
-                        _this['_setResult'](val);
-                        if(typeof val === 'string' && this.insertResults){
-                            this.innerHTML = val;
-                        }
-                        //_this.notifyPath('result');
-                    });
-                    
-                })
+            loadNewUrl(){
+                if(this.href){
+                    const _this = this;
+                    fetch(this.href).then(resp =>{
+                        resp[_this.as]().then(val =>{
+                            _this['_setResult'](val);
+                            if(typeof val === 'string' && this.insertResults){
+                                this.innerHTML = val;
+                            }
+                            //_this.notifyPath('result');
+                        });
+                        
+                    })
+                }
             }
+            
         }
-        
+        customElements.define(XtalFetch.is, XtalFetch);
     }
+    function waitForPolymerElement(){if(typeof Polymer === 'undefined' || Polymer.Element === undefined){setTimeout(waitForPolymerElement, 50);return;}
+        initXtalFetch();
+    }
+    waitForPolymerElement();
 
-    customElements.define(XtalFetch.is, XtalFetch);
+    
 }
