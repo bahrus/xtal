c06f343f18e287f6eaa6c3f3a30b4eb6b4481114
Bruce Anderson <anderson.bruce.b@gmail.com>
2017-04-02 13:47:17 -0400

    example 5b

---
+++ src/xtal-checkbox.js
@@ -0,0 +1,54 @@
+var xtal;
+(function (xtal) {
+    var elements;
+    (function (elements) {
+        function initXtalCheckbox() {
+            class XtalCheckbox extends Polymer.Element {
+                constructor() {
+                    super(...arguments);
+                    this.isChecked = false;
+                    this.indeterminate = false;
+                }
+                static get is() { return 'xtal-checkbox'; }
+                static get properties() {
+                    return {
+                        isChecked: {
+                            type: Boolean,
+                            observer: 'checkedChangeHandler',
+                            notify: true,
+                            reflectToAttribute: true,
+                        },
+                        indeterminate: {
+                            type: Boolean,
+                            observer: 'indeterminateChangeHandler',
+                            notify: true,
+                            reflectToAttribute: true,
+                        }
+                    };
+                }
+                checkedChangeHandler(newVal) {
+                    //this.$.checkbox.checked = newVal;
+                }
+                indeterminateChangeHandler(newVal) {
+                    this.$.checkbox.indeterminate = newVal;
+                }
+                handleClickEvent(mouseEvent, ea) {
+                    console.log({ handleClickEvent: { mouseEvent: mouseEvent, ea: ea, checked: this.$.checkbox.checked } });
+                    this.isChecked = this.$.checkbox.checked;
+                    //this.fire('checkbox-checked', this.isChecked);
+                    this.dispatchEvent(new CustomEvent('checkbox-checked', {
+                        isChecked: this.isChecked
+                    }));
+                }
+                ready() {
+                    super.ready();
+                    if (this.indeterminate)
+                        this.$.checkbox.indeterminate = true;
+                }
+            }
+            customElements.define(XtalCheckbox.is, XtalCheckbox);
+        }
+        initXtalCheckbox();
+    })(elements = xtal.elements || (xtal.elements = {}));
+})(xtal || (xtal = {}));
+//# sourceMappingURL=xtal-checkbox.js.map
---
+++ src/xtal-checkbox.ts
@@ -0,0 +1,49 @@
+module xtal.elements{
+    interface IXtalCheckboxProperties{
+        isChecked: boolean | polymer.PropObjectType,
+        indeterminate: boolean | polymer.PropObjectType,
+    }
+    function initXtalCheckbox(){
+        class XtalCheckbox extends Polymer.Element implements IXtalCheckboxProperties{
+            static get is(){return 'xtal-checkbox';}
+            static get properties() : IXtalCheckboxProperties{
+                return {
+                    isChecked: {
+                        type: Boolean,
+                        observer: 'checkedChangeHandler',
+                        notify: true,
+                        reflectToAttribute: true,
+                    },
+                    indeterminate:{
+                        type: Boolean,
+                        observer: 'indeterminateChangeHandler',
+                        notify: true,
+                        reflectToAttribute: true,
+                    }
+                };
+            }
+            isChecked: boolean = false;
+            indeterminate: boolean = false;
+            checkedChangeHandler(newVal){
+                //this.$.checkbox.checked = newVal;
+            }
+            indeterminateChangeHandler(newVal){
+                this.$.checkbox.indeterminate = newVal;
+            }
+            handleClickEvent(mouseEvent, ea){
+                console.log({handleClickEvent: {mouseEvent: mouseEvent, ea: ea, checked: this.$.checkbox.checked}})
+                this.isChecked = this.$.checkbox.checked;
+                //this.fire('checkbox-checked', this.isChecked);
+                this.dispatchEvent(new CustomEvent('checkbox-checked', {
+                    isChecked: this.isChecked
+                } as CustomEventInit));
+            }
+            ready(){
+                super.ready()
+                if(this.indeterminate) this.$.checkbox.indeterminate = true;
+            }
+        }
+        customElements.define(XtalCheckbox.is, XtalCheckbox);
+    }
+    initXtalCheckbox();
+}
