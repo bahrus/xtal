e05f899676bc9468b716a3d46367f5da7eb67499
Bruce Anderson <anderson.bruce.b@gmail.com>
2017-04-16 13:25:19 -0400

    xtal transformer

--- src/js-transformer.ts
+++ src/js-transformer.ts
@@ -12,101 +12,113 @@ module xtal.elements{
         argument: JSTransformerArgument | polymer.PropObjectType,
         
     }
-    
-    class JSTransformer extends Polymer.Element implements JSTransformerProperties{
-        static get is(){return 'js-transformer';}
-        static get properties() : JSTransformerProperties{
-            return {
-                /**
-                 * The expression to observe and transform when it changes.
-                 */
-                watch:{
-                    type: Object,
-                    observer: 'onPropsChange'
-                },
-                /**
-                 * The expression for where to place the result.
-                 */
-                result:{
-                    type: Object,
-                    notify: true,
-                    readOnly: true
-                },
-                /**
-                 * Configuration argument to pass to the transformer
-                 */
-                argument:{
-                    type: Object
-                },
-                
-            }
-        }
-        watch: object;
-        result: object;
-        argument: JSTransformerArgument;
-        wrapObjectWithPath: string;
+    customElements.whenDefined('xtal-ball').then(() =>{
         /**
-        * Fired when a transform is in progress.
-        *
-        * @event transform
+        * '<js-transformer>' is a Polymer based custom element, that watches for a property of the parent custom element,
+        * applies either a JavaScript method in the parent custom element via event handling, 
+        * and / or it transforms the object based on a global (namespaced)
+        * common function contained within the tag (use hidden attribute)
         */
-        _transformerFns: Function[];
-      
-
-
-
-        onWatchChange(newVal){
-            if(!this._transformerFns){
-                try{
-                    this._transformerFns = eval(this.innerText);
-                }catch(e){
-                    console.error("Unable to parse " + this.innerText);
+        class JSTransformer extends Polymer.Element implements JSTransformerProperties{
+            static get is(){return 'js-transformer';}
+            static get properties() : JSTransformerProperties{
+                return {
+                    /**
+                     * The expression to observe and transform when it changes.
+                     */
+                    watch:{
+                        type: Object,
+                        observer: 'onPropsChange'
+                    },
+                    /**
+                     * The expression for where to place the result.
+                     */
+                    result:{
+                        type: Object,
+                        notify: true,
+                        readOnly: true
+                    },
+                    /**
+                     * Configuration argument to pass to the transformer
+                     */
+                    argument:{
+                        type: Object
+                    },
+                    
                 }
             }
-            
-            var arg = this.argument;
-            if(!arg){
-                arg = {};
-            };
-            arg.context = {
-                element: this,
-                count: 0,
-            };
-            var detail = {obj: newVal, arg: arg};
-            //this.fire('transform', detail);
-            this.dispatchEvent(new CustomEvent('transform', detail))
-            arg.context.count++;
-            let transformedObj = detail.obj;
-            if(this._transformerFns ){
-                for(let i  = 0, ii = this._transformerFns.length; i < ii; i++)
-                {
-                    const transformerFnOrObj = this._transformerFns[i];
-                    switch(typeof(transformerFnOrObj)){
-                        case 'function':
-                            transformedObj = transformerFnOrObj(transformedObj, arg);
-                            break;
-                        default:
-                            throw 'TODO:  error message'
+            watch: object;
+            result: object;
+            argument: JSTransformerArgument;
+            wrapObjectWithPath: string;
+            /**
+            * Fired when a transform is in progress.
+            *
+            * @event transform
+            */
+            _transformerFns: Function[];
+        
+
+
+
+            onPropsChange(newVal){
+                if(!this._transformerFns){
+                    try{
+                        this._transformerFns = eval(this.innerText);
+                    }catch(e){
+                        console.error("Unable to parse " + this.innerText);
                     }
-                    arg.context.count++;
-
                 }
+                
+                var arg = this.argument;
+                if(!arg){
+                    arg = {};
+                };
+                arg.context = {
+                    element: this,
+                    count: 0,
+                };
+                var detail = {obj: newVal, arg: arg};
+                //this.fire('transform', detail);
+                this.dispatchEvent(new CustomEvent('transform', {
+                    detail: detail
+                }));
+                arg.context.count++;
+                let transformedObj = detail.obj;
+                if(this._transformerFns ){
+                    for(let i  = 0, ii = this._transformerFns.length; i < ii; i++)
+                    {
+                        const transformerFnOrObj = this._transformerFns[i];
+                        switch(typeof(transformerFnOrObj)){
+                            case 'function':
+                                transformedObj = transformerFnOrObj(transformedObj, arg);
+                                break;
+                            default:
+                                throw 'TODO:  error message'
+                        }
+                        arg.context.count++;
+
+                    }
+                }
+
+
+                detail.obj = transformedObj;
+                this.dispatchEvent(new CustomEvent('transform', {
+                    detail: detail
+                }));
+                delete arg.context;
+                this['_setResult'](detail.obj);
+
             }
 
-
-            detail.obj = transformedObj;
-            this.dispatchEvent(new CustomEvent('transform', detail));
-            delete arg.context;
-            this['_setResult'](transformedObj);
-
+            connectedCallback(){
+                super.connectedCallback();
+                
+                if(!Array.isArray(this._transformerFns)) delete this._transformerFns;
+            }
         }
 
-        connectedCallback(){
-            super.connectedCallback();
-            
-            if(!Array.isArray(this._transformerFns)) delete this._transformerFns;
-        }
-    }
+        customElements.define(JSTransformer.is, JSTransformer);
+    })
 
-    customElements.define(JSTransformer.is, JSTransformer);
 }
