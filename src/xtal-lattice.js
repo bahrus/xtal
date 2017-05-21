25ebe53f1912efcc266a697bbb847e6d107114e5
Bruce Anderson <anderson.bruce.b@gmail.com>
2017-05-21 14:25:12 -0400

    xtal lattice

--- src/xtal-lattice.js
+++ src/xtal-lattice.js
@@ -3,7 +3,42 @@ var xtal;
     var elements;
     (function (elements) {
         class XtalLattice extends HTMLElement {
+            constructor() {
+                super();
+                this._data = this.data;
+            }
             connectedCallback() {
+                if (this._data !== undefined)
+                    this.updateDOM();
+            }
+            get data() {
+                return this._data;
+            }
+            set data(newVal) {
+                this._data = newVal;
+                if (this._data !== undefined)
+                    this.updateDOM();
+            }
+            updateDOM() {
+                if (this._els === undefined) {
+                    this._els = [];
+                    const els = this._els;
+                    this.querySelectorAll('[data-rc]').forEach((el) => {
+                        const rc = el.dataset.rc;
+                        const rcArr = rc.split(',').map(s => parseInt(s));
+                        const rowNum = rcArr[0];
+                        if (els[rowNum] === undefined)
+                            els[rowNum] = [];
+                        els[rowNum][rcArr[1]] = el;
+                    });
+                }
+                this._data.forEach((row, idx) => {
+                    row.forEach((cell, jdx) => {
+                        const el = this._els[idx][jdx];
+                        console.log({ el: el, cell: cell });
+                        el.innerText = cell;
+                    });
+                });
             }
         }
         customElements.define('xtal-lattice', XtalLattice);
--- src/xtal-lattice.ts
+++ src/xtal-lattice.ts
@@ -1,6 +1,43 @@
 module xtal.elements{
     class XtalLattice extends HTMLElement{
+        _data : any[][];
+        _els : HTMLElement[][];
+        constructor(){
+            super();
+            this._data = this.data;
+        }
         connectedCallback(){
+            if(this._data !== undefined) this.updateDOM();
+        }
+        
+        get data(){
+            return this._data;
+        }
+        set data(newVal){
+            this._data = newVal;
+            if(this._data !== undefined) this.updateDOM();
+        }
+        updateDOM(){
+            if(this._els === undefined){
+                this._els = [];
+                const els = this._els;
+                this.querySelectorAll('[data-rc]').forEach((el: HTMLElement) =>{
+                    const rc = el.dataset.rc
+                    const rcArr = rc.split(',').map(s => parseInt(s));
+                    const rowNum = rcArr[0];
+                    if(els[rowNum] === undefined) els[rowNum] = [];
+                    els[rowNum][rcArr[1]] = el;
+                })
+                
+            }
+            this._data.forEach((row, idx)=>{
+                row.forEach((cell, jdx) =>{
+                    const el = this._els[idx][jdx];
+                    console.log({el: el, cell: cell});
+                    el.innerText = cell;
+                })
+            });
+            
         }
     }
     customElements.define('xtal-lattice', XtalLattice)
