ddf8399b38341ecf086241159668e6f0c4927497
Bruce Anderson <anderson.bruce.b@gmail.com>
2017-05-29 11:24:41 -0400

    xtal defer

--- src/xtal-defer.js
+++ src/xtal-defer.js
@@ -3,14 +3,11 @@ var xtal;
     var elements;
     (function (elements) {
         function initXtalDefer() {
-            console.log('in initXtalDefer');
             class XtalDefer extends Polymer.Element {
-                //static alreadyApplied : {[key: string] : boolean} = {};
                 static get is() { return 'xtal-defer'; }
                 connectedCallback() {
                     super.connectedCallback();
                     const dependencies = this.querySelectorAll('[is="dependency"]');
-                    console.log('dependencies count = ' + dependencies.length);
                     for (let i = 0, ii = dependencies.length; i < ii; i++) {
                         const dependency = dependencies[i];
                         const tagName = dependency.tagName.toLowerCase();
@@ -18,23 +15,29 @@ var xtal;
                     }
                 }
                 processTag(tagName) {
-                    //if(XtalDefer.alreadyApplied[tagName]) return;
-                    //XtalDefer.alreadyApplied[tagName] = true;
+                    if (XtalDefer.alreadyApplied[tagName])
+                        return;
+                    XtalDefer.alreadyApplied[tagName] = true;
                     customElements.whenDefined(tagName).then(() => {
-                        const mixinClass = customElements.get(tagName);
                         const posOfLastSlash = tagName.lastIndexOf('-');
                         const superClassTagName = tagName.substr(0, posOfLastSlash);
                         customElements.whenDefined(superClassTagName).then(() => {
-                            const superClass = customElements.get(superClassTagName);
-                            //for(const key in mixinClass.getOwnProperties)
-                            debugger;
+                            const superClass = customElements.get(superClassTagName).prototype;
+                            const mixinClass = customElements.get(tagName).prototype;
+                            //debugger;
+                            for (const key of Object.getOwnPropertyNames(mixinClass)) {
+                                console.log(key);
+                                if (key === 'constructor')
+                                    continue;
+                                superClass[key] = mixinClass[key];
+                            }
                         });
                     });
                 }
             }
+            XtalDefer.alreadyApplied = {};
             customElements.define(XtalDefer.is, XtalDefer);
         }
-        console.log('iah');
         customElements.whenDefined('xtal-ball').then(() => initXtalDefer());
     })(elements = xtal.elements || (xtal.elements = {}));
 })(xtal || (xtal = {}));
--- src/xtal-defer.ts
+++ src/xtal-defer.ts
@@ -1,15 +1,13 @@
 module xtal.elements{
 
     function initXtalDefer(){
-        console.log('in initXtalDefer');
         class XtalDefer extends Polymer.Element{
-            //static alreadyApplied : {[key: string] : boolean} = {};
+            static alreadyApplied : {[key: string] : boolean} = {};
             static get is(){return 'xtal-defer';}
 
             connectedCallback(){
                 super.connectedCallback();
                 const dependencies = this.querySelectorAll('[is="dependency"]');
-                console.log('dependencies count = ' + dependencies.length);
                 for(let i = 0, ii = dependencies.length; i < ii; i++){
                     const dependency = dependencies[i];
                     const tagName = dependency.tagName.toLowerCase();
@@ -18,22 +16,26 @@ module xtal.elements{
             }
 
             processTag(tagName: string){
-                //if(XtalDefer.alreadyApplied[tagName]) return;
-                //XtalDefer.alreadyApplied[tagName] = true;
+                if(XtalDefer.alreadyApplied[tagName]) return;
+                XtalDefer.alreadyApplied[tagName] = true;
                 customElements.whenDefined(tagName).then(() =>{
-                    const mixinClass = customElements.get(tagName);
+                    
                     const posOfLastSlash = tagName.lastIndexOf('-');
                     const superClassTagName = tagName.substr(0, posOfLastSlash);
                     customElements.whenDefined(superClassTagName).then(() =>{
-                        const superClass = customElements.get(superClassTagName);
-                        //for(const key in mixinClass.getOwnProperties)
-                        debugger;
+                        const superClass = customElements.get(superClassTagName).prototype;
+                        const mixinClass = customElements.get(tagName).prototype;
+                        //debugger;
+                        for(const key of Object.getOwnPropertyNames(mixinClass)){
+                            console.log(key);
+                            if(key === 'constructor') continue;
+                            superClass[key] = mixinClass[key];
+                        }
                     }) 
                 });
             }
         }
         customElements.define(XtalDefer.is, XtalDefer);
     }
-    console.log('iah');
     customElements.whenDefined('xtal-ball').then(()=> initXtalDefer());
 }
